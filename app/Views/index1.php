e715ca0676dc075e4177e266dd77975c610f50e8
Design <dmercadoff@gmail.com>
2021-06-12 22:59:34 -0400

    subiendo fast

--- app/Views/index1.php
+++ app/Views/index1.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Olimpiadas Tokyo 2020</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+
     <style>
         @charset "utf-8";
 * {
@@ -104,8 +106,6 @@ a {
 <head>
 <meta charset="utf-8">
 <title>Olimpiadas Tokyo 2020</title>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="style.css"/>
 </head>
 
 <body>
