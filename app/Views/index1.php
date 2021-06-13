f1a0c861b5c57c286d1ccfb5eae20196abfd925b
Design <dmercadoff@gmail.com>
2021-06-12 22:49:21 -0400

    subiendo fast

---
+++ app/Views/index1.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olimpiadas Tokyo 2020</title>
+    <style>
+        @charset "utf-8";
+* {
+	padding: 0px;
+	margin: 0px;
+}
+body {
+	font-family: 'Work Sans', sans-serif;
+}
+h1, h2, h3, h4, h5, h6 {
+	font-family: 'Oleo Script Swash Caps', cursive;
+}
+a {
+	outline: none !important;
+}
+.navbar-brand {
+	font-family: 'Oleo Script Swash Caps', cursive;
+	color: #ffffff;
+	font-size: 26px;
+}
+.banner {
+	background-attachment: fixed;
+	background-color: #645862;
+	background-image: url(imagenes/olimpiadaasdetokio.jpg);
+	background-position: top center;
+	background-repeat: no-repeat;
+	background-size: cover;
+	color: white;
+	cursor: default;
+	text-align: center;
+	width: 100%;
+	float: left;
+}
+.bg-overlay {
+	padding: 280px 0 220px;
+	background-color: RGBA(15, 5, 20, 0.84);
+}
+.banner-text h2 {
+	font-size: 50px;
+}
+.banner-text h2 span {
+	color: #EB233C;
+}
+.banner-text p {
+	font-size: 15px;
+}
+.mynav {
+	transition: all 0.8s ease-in;
+	background: rgb(68, 51, 68) none repeat scroll 0% 0%;
+}
+.mynav li a {
+	font-family: 'Work Sans', sans-serif;
+	font-size: 14px;
+	font-weight: 400;
+}
+
+.icon-box {
+	position: relative;
+	width: 70px;
+	height: 70px;
+	margin: 0px;
+	display: inline-box;
+	border: rgba(204, 204, 204, 0.18) 1px solid;
+	;
+	border-radius: 50%;
+	padding: 0px !important;
+}
+
+.portfolio {
+	padding: 50px 0;
+	background-color: rgba(235, 213, 235, 0.26);
+	float: left;
+	width: 100%;
+}
+.portfolio-item {
+	margin-bottom: 30px;
+}
+.portfolio h3 {
+	margin-top: 0px;
+	margin-bottom: 50px;
+	border-bottom: 2px solid #EB233C;
+	display: inline-block;
+	line-height: 35px;
+}
+
+.view-more:hover {
+	text-decoration: none;
+	color: #ffffff;
+}
+
+.owl-theme .owl-controls .owl-page span {
+	background-color: #EB233C !important;
+}
+    </style>
+</head>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Olimpiadas Tokyo 2020</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+
+<body>
+<div class="main-header" id="main-header">
+  <nav class="navbar mynav navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <a class="navbar-brand" href="#">Olimpiadas Tokyo 2020</a> </div>
+    </div>
+  </nav>
+</div>
+<div class="banner" id="banner">
+  <div class="bg-overlay">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="banner-text">
+            <h2>Olimpiadas<span>Tokyo 2020</span><h3>En 1964, Tokio se convirtió en la primera ciudad asiática en ser sede de los Juegos Olímpicos, 
+        algo que impulsó a Japón a la era moderna. Ahora, la ciudad se está preparando de nuevo para albergar los JJ.OO. 
+        en el verano de 2021, exactamente un año después de la fecha prevista para los Juegos Olímpicos de Tokio 2020.</h3></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="features">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="feature-box media">
+          <div class="icon-box text-center pull-left media-object"> <i class="icon-bulb"></i> </div>
+          <div class="feature-text media-body">
+            <h4>Fechas importantes de Tokio 2020</h4>
+            <p class="feature-detail">Los juegos de Tokio se llevarán a cabo durante 17 días llenos de acción, concretamente del 24 de julio al 8 de agosto de 2021. La ceremonia de apertura se llevará a cabo en el nuevo Estadio Olímpico de Tokio. Sin embargo, la actividad deportiva comenzará 2 días antes con los primeros partidos de fútbol y softbol. Las rondas clasificatorias para remo y tiro con arco comenzarán, previsiblemente, el día de la ceremonia.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="feature-box media pull-left">
+          <div class="icon-box text-center pull-left media-object"> <i class="icon-eye"></i> </div>
+          <div class="feature-text media-body">
+            <h4>Prohibición de entrada para visitantes extranjeros</h4>
+            <p class="feature-detail">Después de considerarlo detenidamente, el gobierno japonés ha impuesto la prohibición de entrada a visitantes extranjeros en los Juegos Olímpicos de Tokio para frenar la propagación del COVID-19 y prevenir otra oleada de coronavirus en Japón. El COI ha anunciado que respeta plenamente la difícil decisión y el presidente Thomas Bach ha dicho que “debe primar la seguridad y la salud. Cada decisión tiene que respetar, ante todo, el principio de seguridad”.</p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-6 col-xs-12">
+        <div class="feature-box media pull-left">
+          <div class="icon-box text-center pull-left media-object"> <i class="icon-heart"></i> </div>
+          <div class="feature-text media-body">
+            <h4>Medidas de seguridad contra el coronavirus</h4>
+            <p class="feature-detail">Para que los Juegos Olímpicos sigan adelante de manera segura en el verano de 2021, el Comité Organizador de Tokio 2020 ha estado trabajando en estrecha colaboración con el (COI) y el Comité Paralímpico Internacional (CPI) para desarrollar una serie de medidas de seguridad contra el coronavirus y un manual de estrategias COVID-19 para los eventos.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="work" class="portfolio">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-10 col-lg-offset-1 text-center text">
+        <h3>Deportes que se van a presentar</h3>
+        <div class="row">
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/natacion.jpg" style="width: 500px; height: 250px;"></a><h1>natacion</h1> </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/tiroarco.jpg" style="width: 500px; height: 250px;"> </a><h1>Tiro al arco</h1> </div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/atletismo.jpg"style="width: 500px;height: 250px;"></a><h1>Atletismo</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/badminton.jpg"style="width: 500px;height: 250px;"></a><h1>Bádminton</h1></div>
+          </div>
+           <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/beisbol.jpg"style="width: 500px;height: 250px;"></a><h1>Béisbol </h1></div>
+          </div>
+           <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/baloncesto.jpg"style="width: 500px;height: 250px;"></a><h1>Baloncesto</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/boxeo.jpg"style="width: 500px;height: 250px;"></a><h1>Boxeo</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/piraguismo.jpg"style="width: 500px;height: 250px;"></a><h1>Piragüismo</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/escalada.jpg"style="width: 500px;height: 250px;"></a><h1>Escalada</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/ciclismo.jpg"style="width: 500px;height: 250px;"></a><h1>Ciclismo</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/hipica.jpg"style="width: 500px;height: 250px;"></a><h1>Hípica</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/esgrima.jpg"style="width: 500px;height: 250px;"></a><h1>Esgrima</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/gockeyhierba.jpg"style="width: 500px;height: 250px;"></a><h1>Hockey sobre hierba</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/futbol.jpg"style="width: 500px;height: 250px;"></a><h1>Fútbol</h1></div>
+          </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/golf.jpg"style="width: 500px;height: 250px;"></a><h1>Golf</h1></div>
+          </div>
+        <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/gimnasia.jpg"style="width: 500px;height: 250px;"></a><h1>Gimnasia</h1></div>
+        </div>
+        <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/balonmano.jpg"style="width: 500px;height: 250px;"></a><h1>Balon mano</h1></div>
+        </div>
+       <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/judo.jpg"style="width: 500px;height: 250px;"></a><h1>Judo</h1></div>
+        </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/karate.jpg"style="width: 500px;height: 250px;"></a><h1>Karate</h1></div>
+      </div>
+          <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/pentatlon.jpg"style="width: 500px;height: 250px;"></a><h1>Pentatlón moderno</h1></div>
+          </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/remo.jpg"style="width: 500px;height: 250px;"></a><h1>Remo</h1></div>
+      </div>
+            <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/rugby.jpg"style="width: 500px;height: 250px;"></a><h1>Rugby 7</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/vela.jpg"style="width: 500px;height: 250px;"></a><h1>Vela</h1></div>
+      </div>
+            <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/tiro.jpg"style="width: 500px;height: 250px;"></a><h1>Tiro olímpico</h1></div>
+      </div>
+            <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/Skateboarding.jpg"style="width: 500px;height: 250px;"></a><h1>Skateboarding </h1></div>
+      </div>
+            <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/surf.jpg"style="width: 500px;height: 250px;"></a><h1>Surf</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/tenisdemesa.jpg"style="width: 500px;height: 250px;"></a><h1>Tenis de mesa</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/diego.jpg"style="width: 500px;height: 250px;"></a><h1>Taekwondo</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/tenis.jpg"style="width: 500px;height: 250px;"></a><h1>Tenis</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/triatlon.jpg"style="width: 500px;height: 250px;"></a><h1>Triatlón</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/Voleibol.jpg"style="width: 500px;height: 250px;"></a><h1>Voleibol</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/levantamientodePesas.jpg"style="width: 500px;height: 250px;"></a><h1>Levantamiento de pesas</h1></div>
+      </div>
+      <div class="col-md-6 col-sm-6">
+            <div class="portfolio-item"> <a href="#"> <img class="img-portfolio img-responsive" src="imagenes/luchitas.jpg"style="width: 500px;height: 250px;"></a><h1>Lucha</h1></div>
+      </div>
+    </div>
+  </div>
+  <!-- /.container --> 
+</div>
+<div class="about" id="about">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        <h3>Mascotas de Tokio 2020</h3>
+      </div>
+      <div class="col-md-6">
+        <div class="about-text">
+          <br>
+          <br>
+          <br>
+          <br><h4>La mascota de Tokio 2020 es un personaje futurista de color azul llamado Miraitowa. Tiene el emblema de Tokio 2020 en su cabeza y en su cuerpo. El proceso de diseño incluyó a 5 millones de niños en todo Japón que dieron su opinión y aprobación a las diferentes propuestas.
+          La personalidad de Miraitowa se deriva de un proverbio tradicional japonés que significa aprender cosas antiguas y obtener nuevos conocimientos de ellas. La mascota tiene un gran sentido de la justicia y tiene el superpoder de moverse a cualquier lugar de forma instantánea.</h4>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="col-md-6 col-sm-6 col-xs-6 block">
+          <div class="counter-item text-center">
+            <img src="imagenes/mascotas.jpg">
+            <div class="timer" data-from="0" data-to="55" data-speed="5000" data-refresh-interval="50"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
